1f9013469918ca66df59a9993a0083afb1f4965b
unknown <cataniafrancoar@gmail.com>
2022-11-02 02:36:59 -0300

    Mensajes al validar campos del Registro

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -2,7 +2,7 @@ export const Colors = {
   primary100: '#F5D0C5',
   primary500: '#774936',
   primary800: '#3C0000',
-  error100: '#F5FDC6',
+  error100: '#FFF05A',
   error300: '#FFADAD',
   error500: '#B00000',
   cancelar: '#FF0A0A',
