dba17747a426e7a83de266566286d74cbe90dbf5
unknown <cataniafrancoar@gmail.com>
2022-11-06 22:34:01 -0300

    Tildes

--- src/util/firebaseErrorMsg.js
+++ src/util/firebaseErrorMsg.js
@@ -1,11 +1,11 @@
 const errors = {
   authErrors: {
-    authInvalidEmail: 'El correo electronico ingresado tiene un formato invalido',
-    authWrongPassword: 'La contraseña ingresada es invalida',
-    authUserNotFound: 'No se encontro ningun usuario con este correo electronico',
-    authEmailAlreadyInUse: 'El correo electronico ingresado ya esta en uso',
-    authWeakPassword: 'La contraseña elegida no es segura, minimo 6 caracteres.',
-    authTooManyRequests: 'El acceso a esta cuenta esta temporalmente deshabilitado, debido a demasiados intentos de ingreso fallidos.'
+    authInvalidEmail: 'El correo electrónico ingresado tiene un formato inválido.',
+    authWrongPassword: 'La contraseña ingresada es inválida.',
+    authUserNotFound: 'No se encontró ningún usuario con este correo electrónico.',
+    authEmailAlreadyInUse: 'El correo electrónico ingresado ya está en uso',
+    authWeakPassword: 'La contraseña elegida no es segura: mínimo 6 caracteres.',
+    authTooManyRequests: 'El acceso a esta cuenta está temporalmente deshabilitado debido a demasiados intentos de ingreso fallidos.'
   }
 }
 
