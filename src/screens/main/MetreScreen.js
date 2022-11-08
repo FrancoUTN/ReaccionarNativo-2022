c0d1b18427e27f015d53c7bbd827869bc95d3e31
HerikU <hdevep@gmail.com>
2022-11-08 02:39:11 -0300

    listas vacias

--- src/screens/main/MetreScreen.js
+++ src/screens/main/MetreScreen.js
@@ -1,49 +1,96 @@
 import { useEffect, useState } from "react";
-import { collection, getFirestore, onSnapshot, query, where } from "firebase/firestore";
+import {
+  collection,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View, Image, Text } from "react-native";
 import LoadingOverlay from "../../components/ui/LoadingOverlay";
 import ItemClienteEnEspera from "../../components/renderizadores/ItemClienteEnEspera";
+import Sizes_ from "../../util/Sizes";
+import { Colors } from "../../constants/styles";
 
 export default function MetreScreen() {
-    const [clientes, setClientes] = useState([]);
+  const [clientes, setClientes] = useState([]);
 
-    useEffect(() => {
-        const db = getFirestore();
-        const q = query(
-            collection(db, 'usuarios'),
-            where('estado', '==', 'en espera')
-        );
-        return onSnapshot(q, qs => {
-            const clientesTraidos = [];
-            qs.forEach( documento => {
-                const objeto = {
-                    id: documento.id,
-                    ...documento.data()
-                };
-                clientesTraidos.push(objeto);
-            });
-            setClientes(clientesTraidos);
-        });
-    }, []);
-
-    if (!clientes) {
-        return <LoadingOverlay message={'Cargando clientes...'} />
-    }
-
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={clientes}
-                renderItem={({ item }) => <ItemClienteEnEspera item={item} />}
-                keyExtractor={item => item.id}
-            />
-        </View>
+  useEffect(() => {
+    const db = getFirestore();
+    const q = query(
+      collection(db, "usuarios"),
+      where("estado", "==", "en espera")
     );
+    return onSnapshot(q, (qs) => {
+      const clientesTraidos = [];
+      qs.forEach((documento) => {
+        const objeto = {
+          id: documento.id,
+          ...documento.data(),
+        };
+        clientesTraidos.push(objeto);
+      });
+      setClientes(clientesTraidos);
+    });
+  }, []);
+
+  if (!clientes) {
+    return <LoadingOverlay message={"Cargando clientes..."} />;
+  }
+
+  return (
+    <View style={{ flex: 1, width: "100%", height: "100%" }}>
+      {clientes.length <= 0 && (
+        <View
+          style={{
+            flex: 1,
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "white",
+          }}
+        >
+          <View
+            style={{
+              width: "50%",
+              height: "50%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={require("../../../assets/emptyList/emptyClient.png")}
+              style={{ width: "100%", height: "100%", resizeMode: "contain" }}
+            />
+          </View>
+
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: Sizes_.normal,
+              color: Colors.primary500,
+            }}
+          >
+            Sin Clientes aun ...
+          </Text>
+        </View>
+      )}
+
+      {clientes.length > 1 && (
+        <FlatList
+          data={clientes}
+          renderItem={({ item }) => <ItemClienteEnEspera item={item} />}
+          keyExtractor={(item) => item.id}
+        />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
+  container: {
+    flex: 1,
+  },
 });
--- src/screens/specific/MenuScreen.js
+++ src/screens/specific/MenuScreen.js
@@ -1,259 +1,248 @@
 import { useEffect, useState } from "react";
-import { addDoc, collection, doc, getDoc, getFirestore, onSnapshot, query } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getFirestore,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
 import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
 import LoadingOverlay from "../../components/ui/LoadingOverlay";
 import ItemProducto from "../../components/renderizadores/ItemProducto";
 import { Colors } from "../../constants/styles";
 import { getAuth } from "firebase/auth";
 
-
 export default function MenuScreen({ navigation }) {
-    const db = getFirestore();
-    const [productos, setProductos] = useState([]);
-    const [productosSeleccionados, setProductosSeleccionados] = useState([]);
-    const [importe, setImporte] = useState(0);
-    const [demora, setDemora] = useState(0);
-    const [pidiendo, setPidiendo] = useState(false);
+  const db = getFirestore();
+  const [productos, setProductos] = useState([]);
+  const [productosSeleccionados, setProductosSeleccionados] = useState([]);
+  const [importe, setImporte] = useState(0);
+  const [demora, setDemora] = useState(0);
+  const [pidiendo, setPidiendo] = useState(false);
 
-    useEffect(() => {
-        const q = query(
-            collection(db, 'productos')
-        );
-        return onSnapshot(q, qs => {
-            const productosTraidos = [];
-            qs.forEach( documento => {
-                const objeto = {
-                    id: documento.id,
-                    ...documento.data()
-                };
-                productosTraidos.push(objeto);
-            });
-            setProductos(productosTraidos);
-        });
-    }, []);
+  useEffect(() => {
+    const q = query(collection(db, "productos"));
+    return onSnapshot(q, (qs) => {
+      const productosTraidos = [];
+      qs.forEach((documento) => {
+        const objeto = {
+          id: documento.id,
+          ...documento.data(),
+        };
+        productosTraidos.push(objeto);
+      });
+      //setProductos(productosTraidos);
+      setProductos([]);
+    });
+  }, []);
 
-    useEffect(
-        () => {
-            let importeAcumulado = 0;
-            let demoraMaxima = 0;
-            productosSeleccionados.forEach(
-                producto => {
-                    const cant = producto.cantidad;
-                    importeAcumulado += producto.producto.precio * cant;
+  useEffect(() => {
+    let importeAcumulado = 0;
+    let demoraMaxima = 0;
+    productosSeleccionados.forEach((producto) => {
+      const cant = producto.cantidad;
+      importeAcumulado += producto.producto.precio * cant;
 
-                    if (producto.producto.tiempoPromedio > demoraMaxima) {
-                        demoraMaxima = producto.producto.tiempoPromedio;
-                    }
-                }
-            );
-            setImporte(importeAcumulado);
-            setDemora(demoraMaxima);
-        }
-    , [productosSeleccionados]);
+      if (producto.producto.tiempoPromedio > demoraMaxima) {
+        demoraMaxima = producto.producto.tiempoPromedio;
+      }
+    });
+    setImporte(importeAcumulado);
+    setDemora(demoraMaxima);
+  }, [productosSeleccionados]);
 
-    function onCantidadModificadaHandler(id, cantidad) {
-        const productoExistente = productosSeleccionados.find(
-            indice => indice.producto.id == id
-        );
-
-        if (productoExistente) {
-            if (cantidad === 0) {
-                setProductosSeleccionados(
-                    productosPrevios => {
-                        return productosPrevios.filter(
-                            indice => (
-                                indice.producto.id !== productoExistente.producto.id
-                            )
-                        );
-                    }
-                );
-            }
-            else {
-                setProductosSeleccionados(
-                    productosPrevios => {
-                        const elIndice = productosPrevios.findIndex(
-                            productoPrevio => (
-                                productoPrevio.producto.id == productoExistente.producto.id
-                            )
-                        );
-
-                        const nuevoProducto = {...productosPrevios[elIndice]};
-                        nuevoProducto.cantidad = cantidad;
-                        productosPrevios[elIndice] = nuevoProducto;
-
-                        return [...productosPrevios]; // Fundamental (para el useEffect).
-                    }
-                );
-                productoExistente.cantidad = cantidad;
-            }
-        }
-        else {
-            if (cantidad === 0) {
-                // Nada
-            }
-            else {
-                const productoNuevo = productos.find(
-                    prod => prod.id == id
-                );
-
-                const auxProductoNuevo = {
-                    ...productoNuevo
-                }
-
-                const objeto = {
-                    producto: auxProductoNuevo,
-                    cantidad: cantidad
-                };
-
-                setProductosSeleccionados(
-                    productosPrevios => {
-                        return [...productosPrevios, objeto];
-                    }
-                );
-            }
-        }
-    }
-
-    async function pedirHandler() {
-        if (productosSeleccionados.length <= 0) {
-            return;
-        }
-
-        let contieneBebidas = false;
-        let contienePlatos = false;
-        productosSeleccionados.forEach(productoSeleccionado => {
-            if (productoSeleccionado.producto.tipo == 'plato') {
-                contienePlatos = true;
-            }
-            else {
-                contieneBebidas = true;
-            }
-        });
-        let contenido = 'bebidas';
-        if (contienePlatos && contieneBebidas) {
-            contenido = 'mixto';
-        }
-        else if (contienePlatos) {
-            contenido = 'platos';
-        }
-        
-        setPidiendo(true);
-        try{
-            const miUid = getAuth().currentUser.uid;
-            const userRef = doc(db, 'usuarios', miUid);
-            const userSnapshot = await getDoc(userRef);
-            const mesaId = userSnapshot.data().mesa;
-            const mesaRef = doc(db, 'mesas', mesaId);
-            const mesaSnapshot = await getDoc(mesaRef);
-    
-            const pedido = {
-                idMesa: mesaId,
-                fotoMesa: mesaSnapshot.data().foto,
-                fecha: new Date(),
-                idCliente: miUid,
-                metaProductos: productosSeleccionados,
-                importe,
-                estado: 'a confirmar',
-                contenido
-            };
-            await addDoc(collection(db, 'pedidos'), pedido);
-            navigation.navigate({
-                name: 'PedidoAgregado'
-            });
-        }
-		catch (error) {
-            navigation.navigate({
-              name: 'Modal',
-              params: { mensajeError: error.message }
-            });
-        }
-    }
-
-    if (productos.length <= 0) {
-        return <LoadingOverlay message={'Cargando productos...'} />
-    }    
-    if (pidiendo) {
-        return <LoadingOverlay message={'Generando pedido...'} />
-    }
-    return (
-        <>
-            <View style={styles.container}>
-                <FlatList
-                    data={productos}
-                    renderItem={({ item }) => (
-                        <ItemProducto
-                            item={item}
-                            onCantidadModificada={onCantidadModificadaHandler}
-                        />
-                    )}
-                    keyExtractor={item => item.id}
-                />
-            </View>            
-            <View style={styles.viewImporte}>
-                <Text style={styles.textImporte}>
-                    Total:  $ { importe }
-                </Text>
-                <View style={styles.viewDemora}>
-                    <Ionicons
-                        name="time-outline"
-                        color='white'
-                        size={26}
-                    />
-                    <Text style={styles.textDemora}>
-                        { demora }'
-                    </Text>
-                </View>
-                <Pressable
-                    style={({ pressed }) => ([styles.pressablePedir, pressed && { opacity: 0.7 }])}
-                    onPress={pedirHandler}
-                >
-                    <Text style={styles.textPedir}>
-                        ¡Pedir!
-                    </Text>
-                </Pressable>
-            </View>
-        </>
+  function onCantidadModificadaHandler(id, cantidad) {
+    const productoExistente = productosSeleccionados.find(
+      (indice) => indice.producto.id == id
     );
+
+    if (productoExistente) {
+      if (cantidad === 0) {
+        setProductosSeleccionados((productosPrevios) => {
+          return productosPrevios.filter(
+            (indice) => indice.producto.id !== productoExistente.producto.id
+          );
+        });
+      } else {
+        setProductosSeleccionados((productosPrevios) => {
+          const elIndice = productosPrevios.findIndex(
+            (productoPrevio) =>
+              productoPrevio.producto.id == productoExistente.producto.id
+          );
+
+          const nuevoProducto = { ...productosPrevios[elIndice] };
+          nuevoProducto.cantidad = cantidad;
+          productosPrevios[elIndice] = nuevoProducto;
+
+          return [...productosPrevios]; // Fundamental (para el useEffect).
+        });
+        productoExistente.cantidad = cantidad;
+      }
+    } else {
+      if (cantidad === 0) {
+        // Nada
+      } else {
+        const productoNuevo = productos.find((prod) => prod.id == id);
+
+        const auxProductoNuevo = {
+          ...productoNuevo,
+        };
+
+        const objeto = {
+          producto: auxProductoNuevo,
+          cantidad: cantidad,
+        };
+
+        setProductosSeleccionados((productosPrevios) => {
+          return [...productosPrevios, objeto];
+        });
+      }
+    }
+  }
+
+  async function pedirHandler() {
+    if (productosSeleccionados.length <= 0) {
+      return;
+    }
+
+    let contieneBebidas = false;
+    let contienePlatos = false;
+    productosSeleccionados.forEach((productoSeleccionado) => {
+      if (productoSeleccionado.producto.tipo == "plato") {
+        contienePlatos = true;
+      } else {
+        contieneBebidas = true;
+      }
+    });
+    let contenido = "bebidas";
+    if (contienePlatos && contieneBebidas) {
+      contenido = "mixto";
+    } else if (contienePlatos) {
+      contenido = "platos";
+    }
+
+    setPidiendo(true);
+    try {
+      const miUid = getAuth().currentUser.uid;
+      const userRef = doc(db, "usuarios", miUid);
+      const userSnapshot = await getDoc(userRef);
+      const mesaId = userSnapshot.data().mesa;
+      const mesaRef = doc(db, "mesas", mesaId);
+      const mesaSnapshot = await getDoc(mesaRef);
+
+      const pedido = {
+        idMesa: mesaId,
+        fotoMesa: mesaSnapshot.data().foto,
+        fecha: new Date(),
+        idCliente: miUid,
+        metaProductos: productosSeleccionados,
+        importe,
+        estado: "a confirmar",
+        contenido,
+      };
+      await addDoc(collection(db, "pedidos"), pedido);
+      navigation.navigate({
+        name: "PedidoAgregado",
+      });
+    } catch (error) {
+      navigation.navigate({
+        name: "Modal",
+        params: { mensajeError: error.message },
+      });
+    }
+  }
+
+  if (productos.length <= 0) {
+    return <LoadingOverlay message={"Cargando productos..."} />;
+  }
+  if (pidiendo) {
+    return <LoadingOverlay message={"Generando pedido..."} />;
+  }
+  return productos.length > 0 ? (
+    <>
+      <View style={styles.container}>
+        <FlatList
+          data={productos}
+          renderItem={({ item }) => (
+            <ItemProducto
+              item={item}
+              onCantidadModificada={onCantidadModificadaHandler}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+      <View style={styles.viewImporte}>
+        <Text style={styles.textImporte}>Total: $ {importe}</Text>
+        <View style={styles.viewDemora}>
+          <Ionicons name="time-outline" color="white" size={26} />
+          <Text style={styles.textDemora}>{demora}'</Text>
+        </View>
+        <Pressable
+          style={({ pressed }) => [
+            styles.pressablePedir,
+            pressed && { opacity: 0.7 },
+          ]}
+          onPress={pedirHandler}
+        >
+          <Text style={styles.textPedir}>¡Pedir!</Text>
+        </Pressable>
+      </View>
+    </>
+  ) : (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <View></View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 9,
-        alignItems: 'center'
-    },
-    viewImporte: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: Colors.primary500,
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    textImporte: {
-        fontSize: 20,
-        color: 'white',
-        fontWeight: '500',
-        marginLeft: 10
-    },
-    pressablePedir: {
-        backgroundColor: Colors.success,
-        paddingVertical: 5,
-        paddingHorizontal: 15,
-        borderRadius: 8,
-        marginRight: 10
-    },
-    textPedir: {
-        fontSize: 20,
-        color: 'white',
-    },
-    viewDemora: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    textDemora: {
-        fontSize: 20,
-        color: 'white',
-        marginLeft: 4
-    }
+  container: {
+    flex: 9,
+    alignItems: "center",
+  },
+  viewImporte: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: Colors.primary500,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textImporte: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "500",
+    marginLeft: 10,
+  },
+  pressablePedir: {
+    backgroundColor: Colors.success,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    marginRight: 10,
+  },
+  textPedir: {
+    fontSize: 20,
+    color: "white",
+  },
+  viewDemora: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textDemora: {
+    fontSize: 20,
+    color: "white",
+    marginLeft: 4,
+  },
 });
