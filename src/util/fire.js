9c65595da4ce143f0792328d9cd31a0b1a3ccafe
unknown <cataniafrancoar@gmail.com>
2023-01-16 15:02:02 -0300

    Actualización 2023: nuevo Firebase

--- src/util/fire.js
+++ src/util/fire.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7_7gJ6uRqvv5-FZUKkHy38Z-k0V8cywc",
-  authDomain: "comanda-94f6a.firebaseapp.com",
-  projectId: "comanda-94f6a",
-  storageBucket: "comanda-94f6a.appspot.com",
-  messagingSenderId: "296597500465",
-  appId: "1:296597500465:web:024b54bd159dcaede21b10"
+  apiKey: "AIzaSyBLdy5d6HkBmocuQEB5tpjoZlPCcfYiESE",
+  authDomain: "comanda-39297.firebaseapp.com",
+  projectId: "comanda-39297",
+  storageBucket: "comanda-39297.appspot.com",
+  messagingSenderId: "294161132798",
+  appId: "1:294161132798:web:bf1af2f2e290156dc149af"
 };
   
 const app = initializeApp(firebaseConfig);
